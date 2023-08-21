ab3b3c7aca5589e45ca27deb11b7fc46fbaff699
Siavash Namvar <snsinahub@github.com>
2023-08-21 13:19:24 -0400

    add

--- src/report.js
+++ src/report.js
@@ -232,7 +232,7 @@ class Report {
 
     // CSV header
     const invitees = [Report.header]
-    console.log("enterprise:", enterprise)
+    console.log("enterprise index.js:", enterprise)
 
     if (enterprise !== '') {
       // get all orgs in the GitHub Enterprise Cloud account first
